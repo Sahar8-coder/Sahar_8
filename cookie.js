7f9de8ca61fc18674066c808048628b04274f951
Sahar_8 <ze.kirilov@gmail.com>
2021-06-13 11:26:02 +0300

    Initial commit

---
+++ cookie.js
@@ -0,0 +1,11 @@
+let cookie_menu = document.querySelector('.cookie');
+let agree = document.querySelector('.agree');
+let close = document.querySelector('.close');
+
+agree.onclick = function () {
+    cookie_menu.classList.add('display-none');
+}
+
+close.onclick = function () {
+    cookie_menu.classList.add('display-none');
+}
